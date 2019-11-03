3ff2d07f645f7ae64410597849fc1c3616e71180
muje2002 <muje2002@gmail.com>
2019-11-03 22:49:14 +0900

    delete unused files

--- read.py
+++
@@ -1,8 +0,0 @@
-import pandas_datareader.data as web
-import datetime
-
-start = datetime.datetime(1985, 1, 1)
-end = datetime.datetime(2019, 8, 30)
-#gs = web.DataReader("000020.KS", "yahoo", start, end)
-web.DataReader("000020.KS", "yahoo").to_csv("D:/__invest__/db/" + "{0}.csv".format('000020'))
-
